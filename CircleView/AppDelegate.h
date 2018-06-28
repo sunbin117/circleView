8ee2fc608f7680a4e91afbbfc8267617673ad5cc
孙斌 <>
2018-06-28 12:03:03 +0800

    first commit

---
+++ CircleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleView
+//
+//  Created by 孙斌 on 2018/6/28.
+//  Copyright © 2018年 孙斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircleView
+//
+//  Created by 孙斌 on 2018/6/28.
+//  Copyright © 2018年 孙斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
